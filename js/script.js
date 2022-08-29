4b7bb28ac19bda499423e929e3a339a6740f006a
Rafael Santos <contactrafaelsantos@gmail.com>
2022-08-29 18:15:44 -0300

    function to solve the equation implemented

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ function displayNumers(val) {
     document.getElementById("field").value += val;
     return val;
 }
+
+function solveCalculus() {
+    let field = document.getElementById("field").value;
+    let solve = eval(field);
+    document.getElementById("field").value = solve;
+    return solve;
+}
